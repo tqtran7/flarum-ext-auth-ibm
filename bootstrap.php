53bca558f07505e0a16f3ef703cbd838c97bd012
root <root@w3dev.rtp.raleigh.ibm.com>
2017-03-28 16:44:19 -0400

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of Flarum.
+ * (c) Thai Tran <tqtran@us.ibm.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Auth\IBM\Listener;
+use Illuminate\Contracts\Events\Dispatcher;
+
+return function (Dispatcher $events) {
+    $events->subscribe(Listener\AddClientAssets::class);
+    $events->subscribe(Listener\AddIBMAuthRoute::class);
+};
